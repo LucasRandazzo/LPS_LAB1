1d22b4311753e3e9edc0b15aaeee92002b66947d
Pedro Henrique Pires Rodrigues <67903358+Credosvaldo@users.noreply.github.com>
2024-08-23 17:04:35 -0300

    feat: user controller skeloton

--- Codigo/back/src/main/java/com/lps/back/controllers/UserController.java
+++ Codigo/back/src/main/java/com/lps/back/controllers/UserController.java
@@ -2,12 +2,21 @@ package com.lps.back.controllers;
 
 import org.springframework.web.bind.annotation.RestController;
 
+import com.lps.back.dtos.message.MessageDTO;
 import com.lps.back.dtos.user.UserLoginDTO;
 import com.lps.back.dtos.user.UserRegisterDTO;
 import com.lps.back.dtos.user.UserTokenDto;
 import com.lps.back.services.UserService;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import org.hibernate.jdbc.Expectations;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties.User;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -20,32 +29,62 @@ public class UserController {
 
     public ResponseEntity<?> login(UserLoginDTO userLoginDTO)
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'login'");
+        try {
+            long result = userService.login(userLoginDTO);
+            return ResponseEntity.ok(result);
+        }
+        catch (Exception e) {
+            var message = new MessageDTO("Error", "Ocorreu um erro inesperado");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+        }
     }
 
     public ResponseEntity<?> register(UserRegisterDTO userRegisterDTO)
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'login'");
+        try {
+            long result = userService.register(userRegisterDTO);
+            return ResponseEntity.ok(result);
+        }
+        catch (Exception e) {
+            var message = new MessageDTO("Error", "Ocorreu um erro inesperado");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+        }
     }
 
     public ResponseEntity<?> startRecoverPasswordProcess(String email)
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'login'");
+        try {
+            userService.startRecoverPasswordProcess(email);
+            return ResponseEntity.noContent().build();
+        }
+        catch (Exception e) {
+            var message = new MessageDTO("Error", "Ocorreu um erro inesperado");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+        }
     }
 
     public ResponseEntity<?> checkRecoverPasswordToken(UserTokenDto userTokenDto)
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'login'");
+        try {
+            boolean result = userService.checkRecoverPasswordToken(userTokenDto);
+            return ResponseEntity.ok(result);
+        }
+        catch (Exception e) {
+            var message = new MessageDTO("Error", "Ocorreu um erro inesperado");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+        }
     }
 
     public ResponseEntity<?> changePassword(UserLoginDTO userLoginDTO)
     {
-        // TODO Auto-generated method stub
-        throw new UnsupportedOperationException("Unimplemented method 'login'");
+        try {
+            userService.changePassword(userLoginDTO);
+            return ResponseEntity.noContent().build();
+        }
+        catch (Exception e) {
+            var message = new MessageDTO("Error", "Ocorreu um erro inesperado");
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(message);
+        }
     }
 
 }
